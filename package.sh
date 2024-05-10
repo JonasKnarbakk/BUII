75f90df28d5106703dec107e8291d5bd0c47340e
Jonas Knarbakk <jonas@knarbakk.no>
2024-05-10 02:23:18 +0200

    add package script

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ..
+
+zip_filename="~/Downloads/BravosUIImprovements$(date '+%Y%m%d%H%M').zip"
+zip -r ${zip_filename} BravosUIImprovements -x '*.editorconfig' '*.luarc.json' '*package.sh' '*.git*'
+echo "created ${zip_filename}"
