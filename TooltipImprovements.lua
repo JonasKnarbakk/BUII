0b3b6aab0d3c23e4cefe12b6be2e2f5f5326caaf
Jonas Knarbakk <jonas@knarbakk.no>
2024-05-10 02:23:35 +0200

    also add expansion tag to ItemRefTooltip type

--- TooltipImprovements.lua
+++ TooltipImprovements.lua
@@ -1,28 +1,30 @@
 local enabled = nil
 
-local function BUII_AddColoredExpLine(name)
+local function BUII_AddColoredExpLine(name, tooltip)
 	if name == "Classic" then
-		GameTooltip:AddLine(name, 1, 1, 1)
+		tooltip:AddLine(name, 1, 1, 1)
 	elseif name == "Burning Crusade" then
-		GameTooltip:AddLine(name, 0, 1, 0)
+		tooltip:AddLine(name, 0, 1, 0)
 	elseif name == "Wrath of the Lich King" then
-		GameTooltip:AddLine(name, 0, 0.8, 1)
+		tooltip:AddLine(name, 0, 0.8, 1)
 	elseif name == "Cataclysm" then
-		GameTooltip:AddLine(name, 0.8, 0.2, 0)
+		tooltip:AddLine(name, 0.8, 0.2, 0)
 	elseif name == "Mists of Pandaria" then
-		GameTooltip:AddLine(name, 0, 1, 0.6)
+		tooltip:AddLine(name, 0, 1, 0.6)
 	elseif name == "Warlords of Draenor" then
-		GameTooltip:AddLine(name, 0.78, 0.61, 0.43)
+		tooltip:AddLine(name, 0.78, 0.61, 0.43)
 	elseif name == "Legion" then
-		GameTooltip:AddLine(name, 0, 0.8, 0)
+		tooltip:AddLine(name, 0, 0.8, 0)
 	elseif name == "Battle for Azeroth" then
-		GameTooltip:AddLine(name, 0.20, 0.39, 0.67)
+		tooltip:AddLine(name, 0.20, 0.39, 0.67)
 	elseif name == "Shadowlands" then
-		GameTooltip:AddLine(name, 0.6, 0.8, 1)
+		tooltip:AddLine(name, 0.6, 0.8, 1)
 	elseif name == "Dragonflight" then
-		GameTooltip:AddLine(name, 0, 1, 0.6)
+		tooltip:AddLine(name, 0, 1, 0.6)
 	elseif name == "Current Season" then
-		 -- ignore current season tag
+    -- Blizz seems to have added a Current Season tag in 10.2.7
+    -- to have the same behaviour as before the patch make it Dragonflight
+		tooltip:AddLine("Dragonflight", 0, 1, 0.6)
 	else
 		print("Missing expansion: " .. name)
 	end
@@ -30,14 +32,14 @@ end
 
 local function BUII_TooltipImprovements_OnTooltipSetItem(tooltip, data)
 	if enabled then
-		if tooltip == GameTooltip then
+		if tooltip == GameTooltip or tooltip == ItemRefTooltip then
 			local item = select(3, tooltip:GetItem())
 			if item then
 				local expansionID = select(15, GetItemInfo(item))
 				if expansionID then
 					-- EJ_GetTierInfo needs expansionID + 1 to get the correct expansion
 					local expansionName = EJ_GetTierInfo(expansionID + 1)
-					BUII_AddColoredExpLine(expansionName)
+					BUII_AddColoredExpLine(expansionName, tooltip)
 				end
 			end
 		end
